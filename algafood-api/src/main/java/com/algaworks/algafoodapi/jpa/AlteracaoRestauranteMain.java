be5bb228d498b72b1033bb0a5cd291467992b142
DioonS <itzdiih@gmail.com>
2024-01-06 14:09:41 -0300

    Resolução do desafio - Realizar a mesma tarefa de Cozinha para Restaurante

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/jpa/AlteracaoRestauranteMain.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
+
+public class AlteracaoRestauranteMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
+
+        Restaurante restaurante = new Restaurante();
+        restaurante.setId(1L);
+        restaurante.setNome("IFood");
+        restaurante.setTaxaFrete(BigDecimal.valueOf(1.93));
+
+        restaurante = restauranteRepository.salvar(restaurante);
+
+        System.out.printf("%d - %s", restaurante.getId(), restaurante.getNome());
+    }
+}
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,2 +1,6 @@
 INSERT INTO cozinha (nome) VALUES ('Tailandesa');
 INSERT INTO cozinha (nome) VALUES ('Indiana');
+
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Padaria Bella vista', 0.79);
+INSERT INTO restaurante (nome, taxa_frete) VALUES ('Mc Donalds', 1.39);
+
