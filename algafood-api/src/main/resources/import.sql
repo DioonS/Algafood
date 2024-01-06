623cf7096299ef44fb6ea142e7cdc5b2d9e1f964
DioonS <itzdiih@gmail.com>
2024-01-06 18:59:04 -0300

    Mapeando relacionamento com @ManyToOne

--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,6 +1,7 @@
-INSERT INTO cozinha (nome) VALUES ('Tailandesa');
-INSERT INTO cozinha (nome) VALUES ('Indiana');
+INSERT INTO cozinha (id, nome) VALUES (1, 'Tailandesa');
+INSERT INTO cozinha (id, nome) VALUES (2, 'Indiana');
 
-INSERT INTO restaurante (nome, taxa_frete) VALUES ('Padaria Bella vista', 0.79);
-INSERT INTO restaurante (nome, taxa_frete) VALUES ('Mc Donalds', 1.39);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Padaria Bella vista', 0.79, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Mc Donalds', 1.39, 2);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Food Family', 10.99, 2);
 
