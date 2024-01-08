2942075352f389caee62e4d065eb597e6e353799
DioonS <itzdiih@gmail.com>
2024-01-08 10:36:42 -0300

    Consultando singleton resource com GET e @PathVariable

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/EstadoController.java
@@ -4,6 +4,7 @@ import com.algaworks.algafoodapi.domain.model.Estado;
 import com.algaworks.algafoodapi.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class EstadoController {
     @GetMapping
     public List<Estado> listar() {
         return estadoRepository.listar();
+    }
+
+    @GetMapping("{estadoId}")
+    public Estado buscar(@PathVariable Long estadoId) {
+        return estadoRepository.buscar(estadoId);
     }
 }
