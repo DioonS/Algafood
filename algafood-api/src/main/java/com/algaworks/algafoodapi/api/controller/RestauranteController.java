ec1c5c52140fbbd7dc8260d6f5e2683826d877f7
DioonS <itzdiih@gmail.com>
2024-01-09 11:38:03 -0300

    Desafio - Modelando e implementando a consulta de recursos de Restaurantes

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/RestauranteController.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafoodapi.api.controller;
+
+import com.algaworks.algafoodapi.domain.model.Restaurante;
+import com.algaworks.algafoodapi.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurantes")
+public class RestauranteController {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @GetMapping
+    public List<Restaurante> listar() {
+        return restauranteRepository.listar();
+    }
+
+    @GetMapping("/{restauranteId}")
+    public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+        Restaurante restaurante = restauranteRepository.buscar(restauranteId);
+
+        if (restaurante != null) {
+            return ResponseEntity.ok().body(restaurante);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
